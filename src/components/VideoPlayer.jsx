1c6845f796e22f77f953387283b7910d4e4a1777
Hack Reactor Student <student@hackreactor.com>
2017-08-30 16:26:46 -0500

    getting rickrolled

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,11 +1,13 @@
-var VideoPlayer = () => (
+var VideoPlayer = (props) =>
+(
+console.log('props video id',props.video.id.videoId),
   <div className="video-player">
     <div className="embed-responsive embed-responsive-16by9">
-      <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" allowFullScreen></iframe>
+      <iframe className="embed-responsive-item" src=`https://www.youtube.com/embed/{props.video.id.videoId}?autoplay=1` allowFullScreen></iframe>
     </div>
     <div className="video-player-details">
-      <h3>Video Title</h3>
-      <div>Video Description</div>
+      <h3>Hello</h3>
+      <div>{props.video.snippet.description}</div>
     </div>
   </div>
 );
