6d8dde8c2259db50feb507212fb65382e3cb1086
Hack Reactor Student <student@hackreactor.com>
2017-08-30 16:31:06 -0500

    video player complete, rick got rolled

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -3,10 +3,10 @@ var VideoPlayer = (props) =>
 console.log('props video id',props.video.id.videoId),
   <div className="video-player">
     <div className="embed-responsive embed-responsive-16by9">
-      <iframe className="embed-responsive-item" src=`https://www.youtube.com/embed/{props.video.id.videoId}?autoplay=1` allowFullScreen></iframe>
+      <iframe className="embed-responsive-item" src={`https://www.youtube.com/embed/${props.video.id.videoId}`} allowFullScreen></iframe>
     </div>
     <div className="video-player-details">
-      <h3>Hello</h3>
+      <h3>{props.video.snippet.title}</h3>
       <div>{props.video.snippet.description}</div>
     </div>
   </div>
